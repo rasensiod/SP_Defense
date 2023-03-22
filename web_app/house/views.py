2c6ca8e686bc5da163b29021c64195367ea4e3ae
Rafa Asensio <rafa@Rafas-MacBook-Air-2.local>
2023-03-22 01:47:34 +0000

    added bootstrap, still more to go

--- web_app/house/views.py
+++ web_app/house/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('<h1>Smart House Home Page<h1>')
+    return render(request, 'home.html')
