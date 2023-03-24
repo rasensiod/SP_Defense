59ad6e0f6996c9bf70f9384d80abd33354204108
Rafa Asensio <rafa@rafas-air-2.home>
2023-03-24 17:02:04 +0100

    added nicer visuals

--- web_app/house/urls.py
+++ web_app/house/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='sh-home'),
+    path('about/', views.about, name='sh-about'),
 ]
--- web_app/house/views.py
+++ web_app/house/views.py
@@ -3,3 +3,6 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
