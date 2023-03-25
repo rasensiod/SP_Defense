5f5e7386fbfed51cf190c5328c33f30f1bcabfd3
Rafa Asensio <rafa@rafas-air-2.home>
2023-03-25 19:31:30 +0100

    started DB integration

--- web_app/house/models.py
+++ web_app/house/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Devices(models.Model):
+    device_id = models.IntegerField
+    maker = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
--- web_app/house/views.py
+++ web_app/house/views.py
@@ -4,5 +4,12 @@ from django.http import HttpResponse
 def home(request):
     return render(request, 'home.html')
 
+    sql = "SELECT * FROM house_devices"
+    out = Devices.objects.raw(sql)
+
+    print(posts)
+
+    return render(request, 'home.html', {'data': out})
+
 def about(request):
     return render(request, 'about.html')
