736a98b94cc98ef51a98da2f4c602969094dbd1f
Rafa Asensio <rafa@rafas-air-2.home>
2023-04-01 21:41:25 +0200

    basic sql text box working

--- web_app/house/urls.py
+++ web_app/house/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='sh-home'),
     path('about/', views.about, name='sh-about'),
+    path('search/', views.about, name='sh-search'),
 ]
--- web_app/house/views.py
+++ web_app/house/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Devices
 
 def home(request):
-    return render(request, 'home.html')
 
     sql = "SELECT * FROM house_devices"
     out = Devices.objects.raw(sql)
 
-    print(posts)
+    if request.method == 'POST':
+        search = request.POST.get('textfield', None)    # raw SQL user input
+        search_out = Devices.objects.raw(search)
+        return render(request, 'home.html', {'data': search_out})
+    else:
+        return render(request, 'home.html', {'data': out})
 
-    return render(request, 'home.html', {'data': out})
 
 def about(request):
     return render(request, 'about.html')
