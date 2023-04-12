3f59304cf0b54fc591a7488400338cc0ed538b38
Rafa Asensio <rafa@rafas-air-2.home>
2023-04-12 18:24:42 +0200

    security 3 working well

--- sqli_train.py
+++ sqli_train.py
@@ -1,24 +1,48 @@
 import pandas as pd
+from joblib import dump
 from sklearn.svm import SVC
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score, confusion_matrix
-data = pd.read_csv('/kaggle/input/sql-injection-dataset/Modified_SQL_Dataset.csv')
+
+# load dataset
+data = pd.read_csv('Datasets/Modified_SQL_Dataset.csv')
+
+# Define feature extractor
 vectorizer = CountVectorizer(token_pattern=r'\b\w+\b')
+
+# feature extraction
 X = vectorizer.fit_transform(data['Query'])
 y = data['Label']
+
+# Divide training set and test set
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a support vector machine classifier
 clf = SVC(kernel='linear', random_state=42)
+
+# training model
 clf.fit(X_train, y_train)
+
+# prediction test set
 y_pred = clf.predict(X_test)
+
+# Calculate model performance metrics
 accuracy = accuracy_score(y_test, y_pred)
 recall = recall_score(y_test, y_pred)
 precision = precision_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
 tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-print("accuracy: {:.2f}%".format(accuracy * 100))
-print("recall: {:.2f}%".format(recall * 100))
-print("precision: {:.2f}%".format(precision * 100))
+
+# output performance index
+print("##### Model Performance ####")
+print("Accuracy: {:.2f}%".format(accuracy * 100))
+print("Recall: {:.2f}%".format(recall * 100))
+print("Precision: {:.2f}%".format(precision * 100))
 print("F1-score: {:.2f}%".format(f1 * 100))
-print("specificity: {:.2f}%".format(tn / (tn + fp) * 100))
-print("sensitivity: {:.2f}%".format(tp / (tp + fn) * 100))
+print("Specificity: {:.2f}%".format(tn / (tn + fp) * 100))
+print("Sensitivity: {:.2f}%".format(tp / (tp + fn) * 100))
+
+# Save the model and vectoriser object
+dump(clf, 'web_app/hospital/sqli_ml/model.joblib')
+dump(vectorizer, 'web_app/hospital/sqli_ml/vectorizer.joblib')
--- web_app/hospital/views.py
+++ web_app/hospital/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
-from django.db import connection, DataError, ProgrammingError
+from django.db import connection, DataError, ProgrammingError, OperationalError
+from joblib import load
+from sklearn.feature_extraction.text import CountVectorizer
 from .models import Patients
 
-security = 2
+security = 3
 
 def home(request):
 
     sql = "SELECT * FROM hospital_patients"
     out = Patients.objects.raw(sql)
-    sql_bad = "SELECT * FROM hospital_patients WHERE secret_id=88888"
-    out_bad = Patients.objects.raw(sql_bad)
+    # sql_bad = "SELECT * FROM hospital_patients WHERE secret_id=88888"
+    # out_bad = Patients.objects.raw(sql_bad)
+    out_bad = 0
 
     if request.method == 'POST':
         user_input = request.POST.get('textfield', None)    # user input
@@ -29,10 +32,27 @@ def home(request):
                     cursor.execute(search, (user_input,))
                     search_out = cursor.fetchall()
 
+            elif security == 3:
+                search = ('SELECT * FROM hospital_patients WHERE secret_id=' + user_input)
+                search_out = Patients.objects.raw(search)
+                print(search_out)
+
+                model = load('hospital/sqli_ml/model.joblib')
+                vectorizer = load('hospital/sqli_ml/vectorizer.joblib')
+
+                search_v = vectorizer.transform([search])
+                flag = model.predict(search_v)
+                print(flag)
+
+                if flag == 1:
+                    print("Error: Possible SQL injection detected.")
+                    return render(request, 'home.html', {'data': out_bad, 'security': security})
+            
             return render(request, 'home.html', {'data': search_out, 'security': security})
     
-        except (DataError, ProgrammingError, ValueError) as e:
+        except (DataError, ProgrammingError, ValueError, OperationalError) as e:
             print("Error: Possible SQL injection detected.")
+            print(e)
             return render(request, 'home.html', {'data': out_bad, 'security': security})
     else:
         return render(request, 'home.html', {'data': out, 'security': security})
